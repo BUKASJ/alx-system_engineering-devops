05c8a98d8fb19ab7dbc0fddd40e9bed0d7f12e2a
bukasj <chukwuebukaedej@gmail.com>
2023-04-21 16:07:00 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # install flask -v 2.1.0 from pip3
 
-class flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    unless => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
-  }
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
 }
