2d280aa562d846642cbfd41b7396ea87376d0d48
bukasj <chukwuebukaedej@gmail.com>
2023-04-21 15:42:00 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# install puppet-lint -v 2.1.0
+# install puppet-lint -v 2.1.0 from pip3
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+class flask {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    unless => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+  }
 }
+
+include flask
