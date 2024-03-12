50bf3ebd68a05827344e4635c29683055dcf95c5
BUKAS <chukwuebukaedej@gmail.com>
2024-03-12 14:54:03 +0100

    Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -10,11 +10,3 @@ def match_school(input)
     puts ""
   end
 end
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: ruby script.rb <input>"
-else
-  input = ARGV[0]
-  match_school(input)
-  end
