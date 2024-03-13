043d1d85844b48b8dccd0248338ce47e2dbab874
BUKAS <chukwuebukaedej@gmail.com>
2024-03-13 01:17:56 +0100

    regular expression

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,19 +2,4 @@
 
 # This Ruby script accepts one argument and passes it to a regular expression matching method
 
-def match_expression(input)
-  regex = hb+tn
-  if input.match?(regex)
-    puts "Match found: #{input}"
-  else
-    puts "Match not found: #{input}"
-  end
-end
-
-# Check if an argument is provided
-if ARGV.empty?
-  putc ""
-else
-  input = ARGV[0]
-  match_expression(input)
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
