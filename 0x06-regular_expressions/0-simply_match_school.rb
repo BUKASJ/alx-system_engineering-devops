b0ef23b47675c1fd8ae8e71c94dca35c47c7eb9d
BUKAS <chukwuebukaedej@gmail.com>
2024-03-13 01:10:42 +0100

    regular expression must match School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,19 +2,4 @@
 
 # This Ruby script accepts one argument and passes it to a regular expression matching method
 
-def match_school(input)
-  regex = /School/
-  if input.match?(regex)
-    puts "School"
-  else
-    puts ""
-  end
-end
-
-
-if ARGV.empty?
-  putc ""
-else
-  input = ARGV[0]
-  match_school(input)
-end
+puts ARGV[0].scan(/School/).join
