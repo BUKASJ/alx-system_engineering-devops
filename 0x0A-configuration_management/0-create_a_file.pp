dbceb6c7956b804916c49f1153e4aef174eb136c
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-03 19:28:14 -0700

    configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest creates a file named "school" in the /tmp directory
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
