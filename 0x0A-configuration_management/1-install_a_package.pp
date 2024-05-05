4cea2a2131e86f6978823b6f9023366a5ffa5722
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-04 21:08:47 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # Puppet manifest to reinstall Flask version 2.1.0 using pip3
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'uninstall_flask':
+  command => '/usr/bin/pip3 uninstall -y flask',
+  onlyif  => '/usr/bin/pip3 show flask',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Package['python3-pip'],
 }
