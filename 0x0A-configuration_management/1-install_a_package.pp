1f8bb57e8e23252d809334ddc98a660d2e0ee040
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-29 19:35:09 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,11 @@
-# Puppet manifest to reinstall Flask version 2.1.0 using pip3
+# Puppet manifest to install Flask version 2.1.0 and Werkzeug version 2.0.3 using pip3
 
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'uninstall_flask':
-  command => '/usr/bin/pip3 uninstall -y flask',
-  onlyif  => '/usr/bin/pip3 show flask',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show werkzeug | grep -q "Version: 2.0.3"',
   require => Package['python3-pip'],
 }
